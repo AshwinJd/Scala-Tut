40f1e2033057372069832b13d03fe970cfdcb773
AshwinJd <jaydevashwin7@gmail.com>
2017-07-11 15:05:27 +0530

    Pascal Triangle Done

--- pascal.scala
+++ pascal.scala
@@ -3,15 +3,10 @@ object pascal{
     var x = 5;
     for(i <- 0 to x){
         println();
-        for(j<-0 to i){
+      for(j<-0 to i){
         print(pascalFunc(i,j)+ " ");
       }
     }
   }
-  def pascalFunc(i: Int,j:Int): Int = {
-    if(j==0 || i==j)
-      return 1;
-    else
-      return pascalFunc(i-1,j-1)+pascalFunc(i-1,j);
-  }
+  def pascalFunc(i: Int,j:Int):Int = if(j==0 || i==j) return 1 else return pascalFunc(i-1,j-1)+pascalFunc(i-1,j);
 }
