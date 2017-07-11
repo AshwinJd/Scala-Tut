e616139e6388b58c01defe0e8bad407623d016d3
AshwinJd <jaydevashwin7@gmail.com>
2017-07-11 14:58:44 +0530

    Pascal Triangle Done

---
+++ pascal.scala
@@ -0,0 +1,17 @@
+object pascal{
+  def main(args:Array[String]){
+    var x = 5;
+    for(i <- 0 to x){
+        println();
+        for(j<-0 to i){
+        print(pascalFunc(i,j)+ " ");
+      }
+    }
+  }
+  def pascalFunc(i: Int,j:Int): Int = {
+    if(j==0 || i==j)
+      return 1;
+    else
+      return pascalFunc(i-1,j-1)+pascalFunc(i-1,j);
+  }
+}
